35dc10135197769d9efeca489464c463f3fa6f66
NGUYEN HONG NHAN <nguyenhongnhan9a5@gmail.com>
2025-12-11 22:16:12 +0700

    updated project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars, sort_dict
 
 def get_book_text(path):
     with open(path) as f:
@@ -7,6 +7,18 @@ def get_book_text(path):
 
 def main():
     content = get_book_text("./books/frankenstein.txt")
-    print(get_num_words(content))
+    #print(get_num_words(content))
+    char_dict = get_num_chars(content)
+    sorted_list = sort_dict(char_dict)
+    print(f"""
+============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {get_num_words(content)} total words
+--------- Character Count -------
+          """)
+    for char, num in sorted_list:
+        print(f"{char}: {num}")
+    print("============= END ===============")
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,2 +1,16 @@
 def get_num_words(content):
     return (f"Found {len(content.split())} total words")
+
+def get_num_chars(content):
+    char_letter = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+    char_dict = {char: 0 for char in char_letter}
+    for c in content.lower():
+        if c in char_dict:
+            char_dict[c] += 1
+    return char_dict
+def sort_on(items):
+    return items[num]
+def sort_dict(dictionary):
+    sorted_list = list(dictionary.items())
+    sorted_list.sort(reverse=True, key=lambda x:x[1])
+    return sorted_list
