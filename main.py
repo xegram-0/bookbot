80f1b898e90aba58f1d15cc9164352a5ecece7ff
NGUYEN HONG NHAN <nguyenhongnhan9a5@gmail.com>
2025-12-12 15:36:27 +0700

    a description of your changes here

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from stats import get_num_words, get_num_chars, sort_dict
-
+import sys
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    content = get_book_text("./books/frankenstein.txt")
+    print("Usage: python3 main.py <path_to_book>")
+    content = get_book_text(sys.argv[1])
     #print(get_num_words(content))
     char_dict = get_num_chars(content)
     sorted_list = sort_dict(char_dict)
     print(f"""
 ============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
+Analyzing book found at {sys.argv[1]}...
 ----------- Word Count ----------
 Found {get_num_words(content)} total words
 --------- Character Count -------
